84dc1216c013d71ee582597c1deded7a067fee61
heysabbinah <dubysound@hotmail.com>
2016-03-01 19:46:57 +0100

    add template name et form et textarea pr avoir le texte avc le nom de l'user en connexion

--- challenge2.js
+++ challenge2.js
@@ -12,22 +12,33 @@ Template.messages.helpers({ //here change name by messages because it's the temp
   });
 
  // define a helpers for the template here for update the message type in the textarea
- Template.messages.events ({
+ Template.messages.events({
   'keypress textarea': function(e, instance) {
     if (e.keyCode == 13) { //enter key pressed
       var value = instance.find('textarea').value; 
       instance.find('textarea').value = '';
 
 // ici pr inserer les messages et les voir apparaitre
-    Messages.insert ({ // here the name of the template
+    Messages.insert({ // here the name of the template
       message: value,
       timestamp: new Date(),
-      // user: Meteor.userId()
+      user: Meteor.userId()
     });
   }
 }
 
- })
+ });
+
+ Template.message.helpers //template message pr avoir le nom de l'user avec le message
+    user: function() {
+      return Meteor.users.findOne({_id: this.user});
+
+ }
+ });
+
+ Accounts.ui.config({ // account user interface config
+  passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
+ });
 
 }
 
